6a97f7f25ee9f2613fce593bbb731bc88a3e4711
navleorange <o5080161@gmail.com>
2023-05-27 02:01:11 +0900

    first commit

---
+++ audio/__init__.py
@@ -0,0 +1,5 @@
+from audio import audio_transcriber
+from audio import audio_utils
+from audio import transcription
+from audio import vad_utils
+from audio import whisper_utils 
---
+++ main.py
@@ -0,0 +1,9 @@
+from audio import transcription
+import sys
+
+def main():
+    print(sys.path)
+    transcription.transcription()
+
+if __name__ == "__main__":
+    main()
