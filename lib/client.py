ec5d69a9843fa606dfd7856c0d0c1fbd30a5579d
navleorange <navleorange@gmail.com>
2023-06-04 17:50:57 +0900

    complete: only cui

--- lib/client.py
+++ lib/client.py
@@ -12,7 +12,7 @@ class Client:
         self.port = inifile.getint("connection","port")
         self.buffer = inifile.getint("connection","buffer")
 
-    def connet(self) -> None:
+    def connect(self) -> None:
         self.socket.connect((self.host,self.port))
     
     def receive(self) -> str:
@@ -28,9 +28,30 @@ class Client:
 
         return responses.decode("utf-8")
     
-    def send(self, message:str) -> None:
-        message += "\n"
+    def receive_time_out(self) -> str:
+        responses = b""
 
+        while not util.is_json_complate(responces=responses):
+            try:
+                response = self.socket.recv(self.buffer)
+            except:
+                return None
+            
+            if response == b"":
+                raise RuntimeError("socket connection broken")
+            
+            responses += response
+
+        return responses.decode("utf-8")
+    
+    def set_time_out(self,time) -> None:
+        self.socket.settimeout(time)
+    
+    def restore_time_out(self) -> None:
+        self.socket.setblocking(True)
+    
+    def send(self, message:str) -> None:
+        #message += "\n"
         self.socket.send(message.encode("utf-8"))
 
     def close(self) -> None:
--- main.py
+++ main.py
@@ -1,13 +1,36 @@
-from lib import util
+from lib import (
+    util,
+    client
+)
 util.last_resort()
-from audio import transcription
+from player.agent import Agent
 
 def main():
     config_path = "./res/config.ini"
     inifile = util.check_config(config_path=config_path)
     inifile.read(config_path,"utf-8")
 
-    transcription.transcription(inifile=inifile)
+    name = input("あなたの名前を入力してください！:")
+    agent = Agent(inifile=inifile, name=name)
+
+    print("connecting to server...")
+    connection = client.Client(config_path=config_path)
+    connection.connect()
+
+    #agent.transcriber.set_connection(connection=connection)
+
+    while agent.gameContinue:
+
+        if len(agent.received) == 0:
+            agent.parse_info(receive=connection.receive())
+        
+        agent.get_info()
+        message = agent.action()
+
+        if message != "":
+            connection.send(message=message)
+    
+    connection.close()
 
 if __name__ == "__main__":
     main()
